1869f46ac23cbc2cae823d84cc3af0b2fe925c8d
bvvy <bvvy@qq.com>
2018-03-17 18:38:03 +0800

    dev java-> kotlin

---
+++ src/main/kotlin/com/divide2/auth/user/LoginController.kt
@@ -0,0 +1,12 @@
+package com.divide2.auth.user
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by bvvy on 2018/3/17.
+ * com.divide2.auth.user
+ */
+@RestController
+class LoginController {
+
+}
