388fb8c0a82a5544afea829155c6b79c920181aa
bvvy <bvvy@qq.com>
2018-03-19 22:07:07 +0800

    auth auth

--- src/main/kotlin/com/divide2/auth/AuthApplication.kt
+++ src/main/kotlin/com/divide2/auth/AuthApplication.kt
@@ -2,6 +2,9 @@ package com.divide2.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 
 /**
  * Created by bvvy on 2018/3/17.
@@ -10,7 +13,17 @@ import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
-class AuthApplication
+@RestController
+class AuthApplication {
+
+    @GetMapping("/user")
+    fun user(user: Principal): Principal {
+        return user
+    }
+
+
+}
+
 
 fun main(args: Array<String>) {
     runApplication<AuthApplication>(*args)
---
+++ src/main/kotlin/com/divide2/auth/config/LoginConfig.kt
@@ -0,0 +1,14 @@
+package com.divide2.auth.config
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+/**
+ * Created by bvvy on 2018/3/18.
+ * com.divide2.auth.config
+ */
+class LoginConfig : WebSecurityConfigurerAdapter() {
+
+    
+}
